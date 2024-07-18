6f5b192c3da16f7b23bf5c311b6fb847d59684a2
JeyBee <janbjorge@gmail.com>
2024-07-18 07:59:01 +0200

    Remove superfluous use of dsn func

--- tools/consumer.py
+++ tools/consumer.py
@@ -1,13 +1,13 @@
 from __future__ import annotations
 
 import asyncpg
-from PgQueuer.db import AsyncpgDriver, dsn
+from PgQueuer.db import AsyncpgDriver
 from PgQueuer.models import Job
 from PgQueuer.qm import QueueManager
 
 
 async def main() -> QueueManager:
-    connection = await asyncpg.connect(dsn())
+    connection = await asyncpg.connect()
     driver = AsyncpgDriver(connection)
     qm = QueueManager(driver)
 
