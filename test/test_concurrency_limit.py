1d3b37eb61de9ead1276bf90b04f02fc44a3082c
JeyBee <janbjorge@gmail.com>
2024-11-05 18:01:15 +0100

    Refactor Job Dequeuing into Async Generator (#174)

--- test/test_concurrency_limit.py
+++ test/test_concurrency_limit.py
@@ -5,12 +5,18 @@ from dataclasses import dataclass, field
 from datetime import timedelta
 from itertools import count
 
+import async_timeout
 import pytest
 
 from pgqueuer.db import Driver
-from pgqueuer.models import Job
+from pgqueuer.models import Job, JobId
 from pgqueuer.qm import QueueManager
-from pgqueuer.queries import Queries
+from pgqueuer.queries import DBSettings, Queries
+
+
+async def _inspect_queue_jobs(jids: list[JobId], driver: Driver) -> list[Job]:
+    sql = f"""SELECT * FROM {DBSettings().queue_table} WHERE id = ANY($1::integer[])"""
+    return [Job.model_validate(x) for x in await driver.fetch(sql, jids)]
 
 
 @dataclass
@@ -41,12 +47,11 @@ async def consumer(qm: QueueManager, tally: Tally, limit: int) -> None:
 async def enqueue(
     queries: Queries,
     size: int,
+    cnt: count = count(),
 ) -> None:
     assert size > 0
-    cnt = count()
-    entrypoints = ["fetch"] * size
     await queries.enqueue(
-        entrypoints,
+        ["fetch"] * size,
         [f"{next(cnt)}".encode() for _ in range(size)],
         [0] * size,
     )
@@ -79,3 +84,48 @@ async def test_max_concurrency(
 
     for tally in tallys:
         assert max_concurrency - 1 <= tally.max_active <= max_concurrency
+
+
+@pytest.mark.parametrize("concurrency_limit", (1, 5, 10))
+@pytest.mark.parametrize("batch_size", (1, 5, 10))
+@pytest.mark.parametrize("dequeue_timeout", (1, 5, 10))
+async def test_concurrency_entrypoint_isolation(
+    apgdriver: Driver,
+    concurrency_limit: int,
+    batch_size: int,
+    dequeue_timeout: float,
+) -> None:
+    event = asyncio.Event()
+    N = concurrency_limit * 1_000
+    await Queries(apgdriver).enqueue(
+        ["fetch_1", "fetch_2"] * N,
+        [None, None] * N,
+        [0, 0] * N,
+    )
+
+    qm = QueueManager(apgdriver)
+
+    @qm.entrypoint("fetch_1", concurrency_limit=concurrency_limit)
+    async def fetch_1(job: Job) -> None:
+        await event.wait()
+
+    @qm.entrypoint("fetch_2", concurrency_limit=concurrency_limit)
+    async def fetch_2(job: Job) -> None:
+        await event.wait()
+
+    async def timer() -> None:
+        async with async_timeout.timeout(10):
+            while len(event._waiters) < 2 * concurrency_limit:
+                await asyncio.sleep(0.001)
+            len_waiter = len(event._waiters)
+            qm.shutdown.set()
+            event.set()
+            assert len_waiter == 2 * concurrency_limit
+
+    await asyncio.gather(
+        timer(),
+        qm.run(
+            batch_size=batch_size,
+            dequeue_timeout=timedelta(seconds=dequeue_timeout),
+        ),
+    )
