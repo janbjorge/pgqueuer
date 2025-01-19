e005601897bfc233e63d4799e3b36a7109be6143
JeyBee <janbjorge@gmail.com>
2025-01-19 19:42:29 +0100

    Use version_scheme: only-version

--- pgqueuer/_version.py
+++ pgqueuer/_version.py
@@ -12,5 +12,5 @@ __version__: str
 __version_tuple__: VERSION_TUPLE
 version_tuple: VERSION_TUPLE
 
-__version__ = version = '0.18.9.dev2+g1ef43ff.d20250119'
-__version_tuple__ = version_tuple = (0, 18, 9, 'dev2', 'g1ef43ff.d20250119')
+__version__ = version = '0.18.8'
+__version_tuple__ = version_tuple = (0, 18, 8)
