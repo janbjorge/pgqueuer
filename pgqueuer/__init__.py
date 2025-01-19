029701b8dab11294874fb3be96fb226e138d015c
JeyBee <janbjorge@gmail.com>
2025-01-19 13:14:11 +0100

    Add __version__ to init

--- pgqueuer/__init__.py
+++ pgqueuer/__init__.py
@@ -5,6 +5,11 @@ from .qm import QueueManager
 from .queries import Queries
 from .sm import SchedulerManager
 
+try:
+    from ._version import version as __version__
+except ImportError:
+    __version__ = "0.0.0"  # Fallback version if _version.py is missing
+
 __all__ = [
     "AsyncpgDriver",
     "AsyncpgPoolDriver",
