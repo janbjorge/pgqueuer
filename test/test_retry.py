d77953ad7e62e5cfda3551b4cc936055d1f2c936
JeyBee <janbjorge@gmail.com>
2025-03-31 12:33:32 +0200

    Heartbate off by 2x

--- test/test_retry.py
+++ test/test_retry.py
@@ -1,10 +1,7 @@
 import asyncio
 import asyncio.selector_events
-from collections import Counter
+from collections import Counter, defaultdict
 from datetime import datetime, timedelta
-
-import async_timeout
-import pytest
 
 from pgqueuer import db
 from pgqueuer.models import Job
@@ -13,251 +10,228 @@ from pgqueuer.qm import QueueManager
 from pgqueuer.types import JobId
 
 
-async def _inspect_queue_jobs(jids: list[JobId], driver: db.Driver) -> list[Job]:
+async def inspect_queued_jobs(jids: list[JobId], driver: db.Driver) -> list[Job]:
     sql = f"""SELECT * FROM {DBSettings().queue_table} WHERE id = ANY($1::integer[])"""
     return [Job.model_validate(x) for x in await driver.fetch(sql, jids)]
 
 
-@pytest.mark.parametrize("N", (2, 4, 8, 16))
-async def test_retry(
-    apgdriver: db.Driver,
-    N: int,
-    retry_timer: timedelta = timedelta(seconds=0.100),
-) -> None:
-    e = asyncio.Event()
-    c = QueueManager(apgdriver)
-    jobids = Counter[JobId]()
+async def test_retry_after_timer_expired(apgdriver: db.Driver) -> None:
+    N = 25
+    retry_timer = timedelta(seconds=0.250)
 
-    @c.entrypoint("fetch", retry_timer=retry_timer)
+    e = asyncio.Event()
+    qm = QueueManager(apgdriver)
+    calls = Counter[JobId]()
+
+    @qm.entrypoint("fetch", retry_timer=retry_timer)
     async def fetch(context: Job) -> None:
-        jobids[context.id] += 1
+        calls[context.id] += 1
         await e.wait()
 
-    await c.queries.enqueue(["fetch"] * N, [None] * N, [0] * N)
+    await qm.queries.enqueue(["fetch"] * N, [None] * N, [0] * N)
 
     async def stop_after() -> None:
-        async with async_timeout.timeout(retry_timer.total_seconds() * 10):
-            while True:
-                if len(jobids) == N and all(v > 1 for v in jobids.values()):
-                    break
-                await asyncio.sleep(0)
+        # Wait for all jobs to be dequeued
+        while len(calls) < N:
+            await asyncio.sleep(0.001)
+
+        # Give the jobs time to retry
+        await asyncio.sleep(retry_timer.total_seconds() * 2)
 
         e.set()
-        c.shutdown.set()
+        qm.shutdown.set()
 
     await asyncio.gather(
-        c.run(dequeue_timeout=timedelta(seconds=0)),
+        qm.run(dequeue_timeout=timedelta(seconds=0)),
         stop_after(),
     )
-    assert len(jobids) == N
-    assert all(v > 1 for v in jobids.values())
+
+    assert len(calls) == N
+    assert all(v > 1 for v in calls.values())
 
 
-async def test_no_retry_on_zero_timer(
-    apgdriver: db.Driver,
-    retry_timer: timedelta = timedelta(seconds=0),
-) -> None:
-    e = asyncio.Event()
-    c = QueueManager(apgdriver)
-    seen = set[datetime]()
+async def test_no_retry_on_zero_timer(apgdriver: db.Driver) -> None:
+    N = 10
+    retry_timer: timedelta = timedelta(seconds=0)
+    event = asyncio.Event()
+    qm = QueueManager(apgdriver)
+    heartbeat = defaultdict[JobId, list[datetime]](list)
 
-    @c.entrypoint("fetch", retry_timer=retry_timer)
+    async def fetch_db_heartbeat(jobid: JobId) -> datetime:
+        rows = await inspect_queued_jobs([jobid], apgdriver)
+        assert len(rows) == 1
+        return rows[0].heartbeat
+
+    @qm.entrypoint("fetch", retry_timer=retry_timer)
     async def fetch(context: Job) -> None:
-        seen.add(context.heartbeat)
-        await e.wait()
+        heartbeat[context.id].append(await fetch_db_heartbeat(context.id))
+        await event.wait()
+        heartbeat[context.id].append(await fetch_db_heartbeat(context.id))
 
-    await c.queries.enqueue(["fetch"], [None], [0])
+    await qm.queries.enqueue(
+        ["fetch"] * N,
+        [None] * N,
+        [0] * N,
+    )
 
     async def until_retry_updated() -> None:
-        await asyncio.sleep(1)
-        c.shutdown.set()
-        e.set()
+        while not heartbeat:
+            await asyncio.sleep(0)
+        event.set()
+        qm.shutdown.set()
 
     await asyncio.gather(
-        c.run(dequeue_timeout=timedelta(seconds=0)),
+        qm.run(dequeue_timeout=timedelta(seconds=0)),
         until_retry_updated(),
     )
 
-    assert len(seen) == 1
+    assert len(heartbeat) == N
+    for k, v in heartbeat.items():
+        assert len(v) == 2
+        assert v[0] == v[1]
 
 
-async def test_heartbeat_updates(
-    apgdriver: db.Driver,
-    retry_timer: timedelta = timedelta(seconds=0.100),
-) -> None:
-    e = asyncio.Event()
-    c = QueueManager(apgdriver)
-    seen = []
+async def test_heartbeat_no_updates(apgdriver: db.Driver) -> None:
+    retry_timer = timedelta(seconds=0.100)
+    event = asyncio.Event()
+    qm = QueueManager(apgdriver)
+    heartbeats = defaultdict[JobId, list[datetime]](list)
 
-    @c.entrypoint("fetch", retry_timer=retry_timer)
+    async def fetch_db_heartbeat(jobid: JobId) -> datetime:
+        rows = await inspect_queued_jobs([jobid], apgdriver)
+        assert len(rows) == 1
+        return rows[0].heartbeat
+
+    @qm.entrypoint("fetch", retry_timer=retry_timer)
     async def fetch(context: Job) -> None:
-        seen.append(context.heartbeat)
-        await e.wait()
+        heartbeats[context.id].append(await fetch_db_heartbeat(context.id))
+        await event.wait()
+        heartbeats[context.id].append(await fetch_db_heartbeat(context.id))
 
-    jids = await c.queries.enqueue(["fetch"], [None], [0])
-    (before,) = await _inspect_queue_jobs(jids, apgdriver)
-    after: None | Job = None
+    await qm.queries.enqueue(["fetch"], [None], [0])
 
-    async def until_retry_updated() -> None:
-        nonlocal after
-        while len(seen) < 2:
+    async def entrypoint_waiter() -> None:
+        while not heartbeats:
             await asyncio.sleep(0)
-        (after,) = await _inspect_queue_jobs(jids, apgdriver)
-        c.shutdown.set()
-        e.set()
+        event.set()
+        qm.shutdown.set()
 
-    async with async_timeout.timeout(retry_timer.total_seconds() * 10):
-        await asyncio.gather(
-            c.run(dequeue_timeout=timedelta(seconds=0)),
-            until_retry_updated(),
-        )
+    await asyncio.gather(
+        qm.run(dequeue_timeout=timedelta(seconds=0)),
+        entrypoint_waiter(),
+    )
 
-    assert after is not None
-    assert after.heartbeat > before.heartbeat
-    assert len(set(seen)) > 1
-
-
-@pytest.mark.parametrize("N", (2, 4))
-async def test_concurrent_retry_jobs(
-    apgdriver: db.Driver,
-    N: int,
-    retry_timer: timedelta = timedelta(seconds=0.100),
-) -> None:
-    e1 = asyncio.Event()
-    e2 = asyncio.Event()
-    c = QueueManager(apgdriver)
-    seen_job1 = Counter[JobId]()
-    seen_job2 = Counter[JobId]()
-
-    @c.entrypoint("fetch1", retry_timer=retry_timer)
-    async def fetch1(context: Job) -> None:
-        seen_job1[context.id] += 1
-        await e1.wait()
-
-    @c.entrypoint("fetch2", retry_timer=retry_timer)
-    async def fetch2(context: Job) -> None:
-        seen_job2[context.id] += 1
-        await e2.wait()
-
-    await c.queries.enqueue(["fetch1", "fetch2"] * N, [None, None] * N, [0, 0] * N)
-
-    async def until_retry_updated() -> None:
-        while True:
-            if (
-                len(seen_job1) == N
-                and all(v > 1 for v in seen_job1.values())
-                and len(seen_job2) == N
-                and all(v > 1 for v in seen_job2.values())
-            ):
-                break
-            await asyncio.sleep(0)
-
-        c.shutdown.set()
-        e1.set()
-        e2.set()
-
-    async with async_timeout.timeout(retry_timer.total_seconds() * 10 * N):
-        await asyncio.gather(
-            c.run(dequeue_timeout=timedelta(seconds=0)),
-            until_retry_updated(),
-        )
-
-    assert len(seen_job1) == N
-    assert all(v > 1 for v in seen_job1.values())
-
-    assert len(seen_job2) == N
-    assert all(v > 1 for v in seen_job2.values())
+    assert len(heartbeats) == 1
+    for k, v in heartbeats.items():
+        assert len(v) == 2
+        assert v[1] == v[0]
 
 
-@pytest.mark.parametrize("N", (2, 4))
-async def test_varying_retry_timers(
-    apgdriver: db.Driver,
-    N: int,
-    retry_timer_short: timedelta = timedelta(seconds=0.200),
-    retry_timer_long: timedelta = timedelta(seconds=0.400),
-) -> None:
+async def test_varying_retry_timers(apgdriver: db.Driver) -> None:
+    retry_timer_short = timedelta(seconds=0.200)
     e_short = asyncio.Event()
+
+    retry_timer_long = timedelta(seconds=0.400)
     e_long = asyncio.Event()
+
     c = QueueManager(apgdriver)
-    seen_short = Counter[JobId]()
-    seen_long = Counter[JobId]()
+    calls = Counter[JobId]()
 
     @c.entrypoint("fetch_short", retry_timer=retry_timer_short)
     async def fetch_short(context: Job) -> None:
-        seen_short[context.id] += 1
+        calls[context.id] += 1
         await e_short.wait()
 
     @c.entrypoint("fetch_long", retry_timer=retry_timer_long)
     async def fetch_long(context: Job) -> None:
-        seen_long[context.id] += 1
+        calls[context.id] += 1
         await e_long.wait()
 
     await c.queries.enqueue(["fetch_short", "fetch_long"], [None, None], [0, 0])
 
-    async def until_retry_updated() -> None:
-        while True:
-            if (
-                seen_short
-                and all(v > 1 for v in seen_short.values())
-                and seen_long
-                and all(v > 1 for v in seen_long.values())
-            ):
-                break
-            await asyncio.sleep(0)
-
-        c.shutdown.set()
+    async def entrypoint_waiter() -> None:
+        while len(calls) < 2:
+            await asyncio.sleep(0.001)
+        await asyncio.sleep(retry_timer_long.total_seconds() * 2)
+        await asyncio.sleep(retry_timer_short.total_seconds() * 2)
         e_short.set()
         e_long.set()
-
-    async with async_timeout.timeout(2):
-        await asyncio.gather(
-            c.run(dequeue_timeout=timedelta(seconds=0)),
-            until_retry_updated(),
-        )
-
-    assert seen_short and all(v > 1 for v in seen_short.values())
-    assert seen_long and all(v > 1 for v in seen_long.values())
-
-
-@pytest.mark.parametrize("N", (2, 4))
-async def test_retry_with_cancellation(
-    apgdriver: db.Driver,
-    N: int,
-    retry_timer: timedelta = timedelta(seconds=0.100),
-) -> None:
-    e = asyncio.Event()
-    c = QueueManager(apgdriver)
-    seen = set[datetime]()
-    call_count = 0
-
-    @c.entrypoint("fetch", retry_timer=retry_timer)
-    async def fetch(context: Job) -> None:
-        nonlocal call_count
-        seen.add(context.heartbeat)
-        call_count += 1
-        if call_count < N:
-            raise asyncio.CancelledError("Simulated cancellation")
-        await e.wait()
-
-    await c.queries.enqueue(["fetch"], [None], [0])
-
-    async def until_retry_updated() -> None:
-        while len(seen) < N:
-            await asyncio.sleep(0)
         c.shutdown.set()
-        e.set()
 
-    async with async_timeout.timeout(retry_timer.total_seconds() * 2 * N):
-        await asyncio.gather(
-            c.run(dequeue_timeout=timedelta(seconds=0)),
-            until_retry_updated(),
-        )
-
-    assert call_count == N
-    assert (
-        pytest.approx(
-            (max(seen) - min(seen)).total_seconds(),
-            abs=retry_timer.total_seconds(),
-        )
-        == (N * retry_timer).total_seconds()
+    await asyncio.gather(
+        c.run(dequeue_timeout=timedelta(seconds=0)),
+        entrypoint_waiter(),
     )
+
+    assert len(calls) == 2
+    assert all(v > 1 for v in calls.values())
+
+
+async def test_retry_with_cancellation(apgdriver: db.Driver) -> None:
+    N = 4
+    retry_timer = timedelta(seconds=0.100)
+    event = asyncio.Event()
+    qm = QueueManager(apgdriver)
+    calls = Counter[JobId]()
+
+    @qm.entrypoint("fetch", retry_timer=retry_timer)
+    async def fetch(context: Job) -> None:
+        calls[context.id] += 1
+        if calls[context.id] < N:
+            raise asyncio.CancelledError("Simulated cancellation")
+        await event.wait()
+
+    await qm.queries.enqueue(["fetch"], [None], [0])
+
+    async def entrypoint_waiter() -> None:
+        while sum(v for v in calls.values()) < N:
+            await asyncio.sleep(0.001)
+        event.set()
+        qm.shutdown.set()
+
+    await asyncio.gather(
+        qm.run(dequeue_timeout=timedelta(seconds=0)),
+        entrypoint_waiter(),
+    )
+
+    assert len(calls) == 1
+    assert sum(v for v in calls.values()) == N
+
+
+async def test_heartbeat_db_datetime(apgdriver: db.Driver) -> None:
+    retry_timer = timedelta(seconds=0.150)
+    event = asyncio.Event()
+    qm = QueueManager(apgdriver)
+
+    async def fetch_db_heartbeat(jobid: JobId) -> timedelta:
+        sql = f"""SELECT NOW() - heartbeat AS dt FROM {DBSettings().queue_table} WHERE id = ANY($1::integer[])"""  # noqa: E501
+        rows = await apgdriver.fetch(sql, [jobid])
+        assert len(rows) == 1
+        return rows[0]["dt"]
+
+    @qm.entrypoint("fetch", retry_timer=retry_timer)
+    async def fetch(context: Job) -> None:
+        await event.wait()
+
+    jid, *_ = await qm.queries.enqueue(["fetch"], [None], [0])
+
+    async def entrypoint_waiter() -> None:
+        await asyncio.sleep(retry_timer.total_seconds() * 2)
+        event.set()
+        qm.shutdown.set()
+
+    async def poller() -> list[timedelta]:
+        samples = list[timedelta]()
+        while not event.is_set():
+            samples.append(await fetch_db_heartbeat(jid))
+        return samples
+
+    samples, *_ = await asyncio.gather(
+        poller(),
+        qm.run(dequeue_timeout=timedelta(seconds=0)),
+        entrypoint_waiter(),
+    )
+
+    leeway = timedelta(milliseconds=1)
+    for sample in samples:
+        assert sample - leeway < retry_timer, (sample, retry_timer, sample - retry_timer)
